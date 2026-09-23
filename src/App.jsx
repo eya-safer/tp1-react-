d2fcf39051c00319af517d86fd5fe549f83e69cf
eya-safer <eya.safer.1@gmail.com>
2026-09-23 12:18:30 +0100

    fix deployment error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Main from './components/Main'
 function App() {
